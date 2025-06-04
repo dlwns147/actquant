819c10d59547bfe02cd5c07cbf2dc4d657c7c838
dlwns147 <lsj000777@naver.com>
2025-06-04 06:59:07 +0000

    update flexible group size

--- search/scripts/awqgptq.sh
+++ search/scripts/awqgptq.sh
@@ -2,12 +2,12 @@ DEVICES=${1}
 TODAY=`date +%y%m%d%H%M`
 PORT_NUM=$(( ( RANDOM % 10000 )  + 10000 ))
 
-# MODEL_PATH=/SSD/huggingface/meta-llama
-# # MODEL_NAME=Llama-2-7b-hf
-# # MODEL_NAME=Llama-2-13b-hf
-# MODEL_NAME=Llama-3.1-8B-Instruct
-# DTYPE=float16
-# CONFIG=config/llama.json
+MODEL_PATH=/SSD/huggingface/meta-llama
+# MODEL_NAME=Llama-2-7b-hf
+# MODEL_NAME=Llama-2-13b-hf
+MODEL_NAME=Llama-3.1-8B-Instruct
+DTYPE=float16
+CONFIG=config/llama.json
 
 # MODEL_PATH=/SSD/huggingface/Qwen
 # # MODEL_NAME=Qwen2.5-7B
@@ -20,12 +20,12 @@ PORT_NUM=$(( ( RANDOM % 10000 )  + 10000 ))
 # DTYPE=float16
 # CONFIG=config/qwen2.json
 
-MODEL_PATH=/SSD/huggingface/mistralai
-# MODEL_NAME=Mistral-7B-v0.3
-MODEL_NAME=Mistral-7B-Instruct-v0.3
-# DTYPE=bfloat16
-DTYPE=float16
-CONFIG=config/mistral.json
+# MODEL_PATH=/SSD/huggingface/mistralai
+# # MODEL_NAME=Mistral-7B-v0.3
+# MODEL_NAME=Mistral-7B-Instruct-v0.3
+# # DTYPE=bfloat16
+# DTYPE=float16
+# CONFIG=config/mistral.json
 
 Q_BITS="2 3 4"
 Q_BITS_TEXT="234"
@@ -94,14 +94,14 @@ CUDA_VISIBLE_DEVICES=${DEVICES} accelerate launch --num_processes=${N_PROC} --nu
 --use_flash \
 -n ${N} \
 --save ${SAVE} \
+--datasets ${DATASETS} \
+--zeroshot \
+--tasks ${TASKS} \
 --long_bench \
 --long_bench_result_path ${LONG_BENCH_RESULT_PATH} \
 --long_bench_config ${LONG_BENCH_CONFIG} \
 --clip_asym
 
-# --datasets ${DATASETS} \
-# --zeroshot \
-# --tasks ${TASKS} \
 
 # --method ${METHOD} \
 # --group_size ${GROUP_SIZE} \
--- search/utils/func.py
+++ search/utils/func.py
@@ -65,7 +65,7 @@ def compute_bits(arch, config, group_size, target='w'):
             #         c_group_size = config['linear_shape'][config['k_linear']][0]
             #     else:
             #         c_group_size = config['linear_shape'][config['v_linear']][0]
-            return np.mean(arch[target]).item() + (32 / group_size[target])
+            return np.mean(arch[target]).item() + (32 / group_size[target][0])
         elif len(group_size[target]) > 1:
             bits_list, group_size_list = [x[0] for x in arch[target]], [x[1] for x in arch[target]]
             return np.mean(bits_list).item() + 32 / np.mean(group_size_list).item()
@@ -76,7 +76,7 @@ def compute_bits(arch, config, group_size, target='w'):
         if len(group_size['k']) == 1:
             # k_group_size = config['linear_shape'][config['k_linear']][0] if group_size['k'] == -1 else group_size['k']
             # v_group_size = config['linear_shape'][config['v_linear']][0] if group_size['v'] == -1 else group_size['v']
-            return np.mean(arch['k'] + arch['v']).item() + 32 / np.mean(group_size['k'] + group_size['v']).item() # (16 / group_size['k']) + (16 / v_group_size)
+            return np.mean(arch['k'] + arch['v']).item() + 32 / np.mean(group_size['k'][0] + group_size['v'][0]).item() # (16 / group_size['k']) + (16 / v_group_size)
         elif len(group_size['k']) > 1:
             k_bits_list, k_group_size_list = [x[0] for x in arch['k']], [x[1] for x in arch['k']]
             v_bits_list, v_group_size_list = [x[0] for x in arch['v']], [x[1] for x in arch['v']]
